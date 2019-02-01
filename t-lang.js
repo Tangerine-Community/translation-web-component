6e477639e97b3d1c9a416e6522f3e353bdd59303
R.J. Steinert <rj@rjsteinert.com>
2019-02-01 14:33:38 -0500

    Add t-lang element, tests, and demo

---
+++ t-lang.js
@@ -0,0 +1,36 @@
+import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
+
+/**
+ * `t-lang`
+ * 
+ *
+ * @customElement
+ * @polymer
+ * @demo demo/index.html
+ */
+class TLang extends PolymerElement {
+  static get template() {
+    return html`
+      <slot></slot>
+    `;
+  }
+  
+  connectedCallback() {
+    super.connectedCallback()
+    this.render()
+    document.body.addEventListener('lang-change', this.render.bind(this))
+  }
+
+  render() {
+    if (document.documentElement.lang.length > 0) {
+      if (!this.attributes.hasOwnProperty(document.documentElement.lang)) {
+        this.style.setProperty('display', 'none')
+      } else {
+        this.style.setProperty('display', 'inline')
+      }
+    }
+  }
+
+}
+
+window.customElements.define('t-lang', TLang);
