ee190cfcb56bf7ec6f16a9a19fff362b921a8b1e
R.J. Steinert <rj@rjsteinert.com>
2019-03-11 12:49:55 -0400

    Rename getTranslation to combTranslations and make safer

--- util.js
+++ util.js
@@ -1,19 +1,24 @@
 
-export function getTranslation(markup, languageCode) {
+export function combTranslations(markup, languageCode) {
   let template = document.createElement('template')
   template.innerHTML = markup
   languageCode = !languageCode ? window.document.lang : languageCode
   languageCode = !languageCode ? 'en' : languageCode
-  return [...template.content.childNodes].map(node => {
-    switch(node.nodeName) {
-      case 'T-LANG':
-        // TODO support uppercase, look at attributes lower cased.
-        return node.hasAttribute(languageCode) ? node.innerHTML : ''
-      case '#text':
-        return node.wholeText
-      default:  
-        return node.outerHTML
+  template.content.querySelectorAll('t-lang').forEach(node => {
+    if (!node.hasAttribute(languageCode)) {
+      node.remove() 
+    } else {
+      unwrap(node)
     }
-  }).join('')
+  })
+  return template.innerHTML
+}
 
+function unwrap(el) {
+  // get the element's parent node
+  var parent = el.parentNode;
+  // move all children out of the element
+  while (el.firstChild) parent.insertBefore(el.firstChild, el);
+  // remove the empty element
+  parent.removeChild(el);
 }
